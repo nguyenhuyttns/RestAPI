5b6a60b2b4e466753102558895450208b8e1ef30
nguyenhuyttns <huy2010200320102003@gmail.com>
2025-02-24 11:49:38 +0700

    adding model to the api calling

--- callApi/lib/screen/home.dart
+++ callApi/lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:firstapp/model/user.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,7 +12,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<dynamic> users = [];
+  List<User> users = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +21,12 @@ class _HomeScreenState extends State<HomeScreen> {
         itemCount: users.length,
         itemBuilder: (context, index) {
           final user = users[index];
-          final name = user['name']['first'];
-          final email = user['email'];
-          final imageUrl = user['picture']['thumbnail'];
+          final color = user.gender == 'male' ? Colors.blue : Colors.green;
+
           return ListTile(
-            leading: ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.network(imageUrl),
-            ),
-            title: Text(name),
-            subtitle: Text(email),
+            title: Text(user.name.first),
+            subtitle: Text(user.phone),
+            // tileColor: color,
           );
         },
       ),
@@ -44,9 +41,25 @@ class _HomeScreenState extends State<HomeScreen> {
     final response = await http.get(uri);
     final body = response.body;
     final json = jsonDecode(body);
-
+    final results = json['results'] as List<dynamic>;
+    final tranformed =
+        results.map((e) {
+          final name = UserName(
+            title: e['name']['title'],
+            first: e['name']['first'],
+            last: e['name']['last'],
+          );
+          return User(
+            gender: e['gender'],
+            email: e['email'],
+            phone: e['phone'],
+            cell: e['phone'],
+            nat: e['nat'],
+            name: name,
+          );
+        }).toList();
     setState(() {
-      users = json['results'];
+      users = tranformed;
     });
     print('fetchUser completed');
   }
